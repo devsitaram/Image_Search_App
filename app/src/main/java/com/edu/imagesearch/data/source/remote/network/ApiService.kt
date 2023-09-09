0e364921a0dea0e48f1133b7064d0ae2ae524073
Sita Ram Thing <sitaramthing667@gmail.com>
2023-09-09 22:42:31 +0545

    create the ApiService interface file in data/source/remote/network package

---
+++ app/src/main/java/com/edu/imagesearch/data/source/remote/network/ApiService.kt
@@ -0,0 +1,16 @@
+package com.edu.imagesearch.data.source.remote.network
+
+import com.edu.imagesearchapp.data.source.remote.pojo.PixabayDTO
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val BASE_URL = "https://pixabay.com/"
+
+// https://pixabay.com/api/?key=39343490-929c50e58ac235a67f48917fa&q=sunflower
+
+interface ApiService {
+
+    @GET("api/")
+    suspend fun getSearchImage(@Query("key") key: String, @Query("q") query: String): PixabayDTO
+
+}
